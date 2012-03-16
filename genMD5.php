e303fa3a83d7ab23ca7a39728a59edef12f0a14e
Jakrapan Cholchalatharn <jak_cholchalatan@hotmail.com>
2012-03-17 00:48:50 +0700

    asdf

---
+++ genMD5.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$username = $_SESSION["user"];
+$start_lat = $_GET["start_lat"];
+$end_lng = $_GET["end_lng"];
+$cn = mysql_connect("localhost:3307","root","adminadmin");
+if (!$cn) {
+    echo "fail<br>";
+    exit;
+}
+mysql_select_db("gps", $cn);
+$sql1 = "select * from trip order by trip_id DESC"; // trip_id
+$result1 = mysql_query($sql1, $cn);
+$row1 = mysql_fetch_array($result1);
+$idtrip = $row1["trip_id"];
+$idtrip += 1;
+$mdd5 = md5($idtrip+$start_lat+$end_lng);
+$authen = substr($mdd5,0,strlen($mdd5)/4);
+
+    
+
+echo($authen);
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
