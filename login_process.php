1a70885037f1e58003c4f6f8804ff36ed55be1af
karamail11 <karamail_11@hotmail.com>
2012-03-26 23:44:03 +0700

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -1,8 +1,22 @@
+<?php session_start() ; ob_start();?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Untitled Document</title>
+<style type = "text/css">
+</style>
+<SCRIPT LANGUAGE="JAVASCRIPT" TYPE="TEXT/JAVASCRIPT"> 
+
+</SCRIPT> 
+</head>
+
+<body>
+
 <?php
 // ใว้ข้างบนสุด ครับ
-ob_start();
-session_start();
-$cn = @mysql_connect("localhost:3307", "root", "adminadmin");
+$cn = @mysql_connect("localhost", "root", "adminadmin");
 if (!$cn) {
     echo "fail<br>";
     exit;
@@ -63,6 +77,14 @@ mysql_select_db("gps", $cn);
             }
 	
 	
+		function check(str){
+			if (str =="admin"){
+				window.document.location.href='GPS2/index2.php';
+			}
+			else{
+				window.document.location.href='GPS2/index-member.php';
+			}
+		}
         </script>
         <title>Home</title>
     </head>
@@ -150,7 +172,7 @@ mysql_select_db("gps", $cn);
                                 </tr>
                             </tbody>
                         </table>
-                        <a href='mail.php' class='link'><input type='submit' value='Go to Web' class="medium blue nice button radius" /></a>
+                        <a href='#' onclick="check('<?php  echo $row["rank"]; ?>')" class='link'><input type='submit' value='Go to Web' class="medium blue nice button radius" /></a>
                         <input type='submit' value='Edit' onclick="edit(<?php echo $row["id"] ?>)" class="medium blue nice button radius" />
                     </div>
                     <div id="editbox" style="display: none;">
@@ -221,16 +243,109 @@ mysql_select_db("gps", $cn);
 
             </div>
 
+
             <?php }
             else {
                 ?>
-                <div id="center" style="margin-left: auto; margin-right: auto;" >
+				<div id="center" style="margin-left: auto; margin-right: auto;" >
                     <div id="header">
 
                     </div>
-                    <div class="style4" id="nevigator">
+					 <div class="style4" id="nevigator">
                         <a href="#" onclick="login()" class="link"> Login</a>
                         |<a href="#" onclick="authen()" class="link" style="margin-right: 20px">  Authentication </a>
+
+<?php
+	$sql = "select * from gps.user";
+	$result = mysql_query($sql,$cn);
+	
+	
+	while($row = mysql_fetch_array($result)){
+		
+		if($username == $row["username"]){
+			if($password ==$row["password"]){
+				if($row["rank"]=="admin"){
+					
+					echo "<div align='left'><h2>Welcome To Admin Page!</h2></div><br>";
+					echo "<table width='250' border='0'><tr><th width='125' scope='row'><div align='left'>Username</div></th>";
+					echo "<td width='109'><div align='left'>".$row["username"]."</div></td></tr>";
+					echo "<tr><th scope='row'><div align='left'>password</div></th>";
+					echo "<td>".$row["password"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>E-Mail</div></th>";
+					echo "<td>".$row["email"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Rank</div></th>";
+					echo "<td>".$row["rank"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Phone</div></th>";
+					echo "<td>".$row["phone"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Address</div></th>";
+					echo "<td>".$row["address"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Company-name</div></th>";
+					echo "<td>".$row["company_name"]."</td></tr></table>";
+					echo "<a href='mail.php' class='link'><input type='submit' value='Go to Web'></a>";
+					echo "<input type='submit' value='Edit!' onclick='edit(".$row["id"].")'>";
+					//echo "<meta http-equiv=\"refresh\" content=\"1; URL= home2.php\">";
+					
+				//echo "<a class='link' >Hi : ".$username ."</a>";
+				//echo "<input type='submit' value='Logout' onclick='logout()' />";	
+				$_SESSION['user'] = $_GET["username"] ;
+				$_SESSION['rank'] = "admin";
+				$_SESSION['company']= $row["company_name"];
+				echo "<script language=javascript>alert('Please enter a valid username.')</script>";
+				
+				}
+				else if($row["rank"]=="member"){
+					echo "<div align='left'><h2>Welcome To Member Page!</h2></div><br>";
+					echo "<table width='250' border='0'><tr><th width='125' scope='row'><div align='left'>Username</div></th>";
+					echo "<td width='109'><div align='left'>".$row["username"]."</div></td></tr>";
+					echo "<tr><th scope='row'><div align='left'>password</div></th>";
+					echo "<td>".$row["password"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>E-Mail</div></th>";
+					echo "<td>".$row["email"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Rank</div></th>";
+					echo "<td>".$row["rank"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Phone</div></th>";
+					echo "<td>".$row["phone"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Address</div></th>";
+					echo "<td>".$row["address"]."</td></tr>";
+					echo "<tr><th scope='row'><div align='left'>Company-name</div></th>";
+					echo "<td>".$row["company_name"]."</td></tr></table>";
+					echo "<a href='member.php' class='link'><input type='submit' value='Go to Web'></a>";
+					echo "<input type='submit' value='Edit!'>";
+					
+					
+				//echo "<a class='link' >Hi : ".$username ."</a>";
+				//echo "<input type='submit' value='Logout' onclick='logout()' />";	
+				$_SESSION['user'] = $_GET["username"] ;
+				$_SESSION['rank'] = $row["rank"];
+				$_SESSION['company']= $row["company_name"];
+				echo "<SCRIPT LANGUAGE='javascript'><!--n";
+	echo "KillMe();n";
+	echo "// --></SCRIPT>";
+				
+				}
+				else{
+				$_SESSION['user'] = $_GET["username"] ;
+				$_SESSION['rank'] = "user";
+				echo "Hi :".$username;
+				echo "<input type='submit' value='Logout' onclick='logout()' />";
+				//echo "<meta http-equiv='refresh' content='2;URL=gps/mail.php#' />";
+				}
+			}
+			else{
+				echo "Password Fail  "."<a href='#' onclick='login()'>Login</a>";
+			}
+		}
+		
+	}
+	
+	/*if($result){
+		echo "HI : " . $username ;
+	}
+	else{
+		echo "fail";
+	}*/
+?>
+</body>
 
                     </div>
                     <center>
@@ -243,5 +358,6 @@ mysql_select_db("gps", $cn);
                     </center>
                 </div>
             <?php } ?>
-    </body>
+    
+
 </html>
